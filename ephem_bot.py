31e9db5a637691ca0365b08c7f76380e9ab7d36e
DmitriyMorgunov <domorgunov@yandex.ru>
2020-02-23 13:07:24 +0300

    Тренируюсь делать коммиты и пр.

--- ephem_bot.py
+++ ephem_bot.py
@@ -13,6 +13,7 @@
 
 """
 import logging
+import settings
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -20,16 +21,6 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
 )
-
-
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn', 
-        'password': 'python'
-    }
-}
-
 
 def greet_user(bot, update):
     text = 'Вызван /start'
@@ -41,10 +32,10 @@ def talk_to_me(bot, update):
     user_text = update.message.text 
     print(user_text)
     update.message.reply_text(user_text)
- 
+
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY)
+    mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
     
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
@@ -52,7 +43,7 @@ def main():
     
     mybot.start_polling()
     mybot.idle()
-       
+
 
 if __name__ == "__main__":
     main()
